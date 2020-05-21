d20b0119b6dfe9a61f1664e01d116dafd4242e4d
Danira Cortez <cortezd334@gmail.com>
2020-05-21 19:58:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,35 +1,44 @@
-def find_min_in_nested_arrays(src)
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
-  count = 0
-  min = []
-  while count < src.length do
-    inner_count = 0
-    value = 10000
-    
-    while inner_count < src[count].length do
-#      if src[count][inner_count] < src[count][inner_count - 1]
-#        value = src[count][inner_count]
-##        min << src[count][inner_count]
-#      end
-#      inner_count += 1
-#    end
-#    min << value
-#    count += 1
-#  end
-#  min
-##  min << value
-#end
-#am getting the last number of each array w/ src[count][inner_count - 1] how do I get the previous element? (instead of last)
-#also don't like that I had to hard code this by setting value to 10000
-
-      if src[count][inner_count] < value
-       value = src[count][inner_count]
-      end
-      inner_count += 1
+def min_from_array(array)
+  current_min = array[0]
+  count = 1
+  
+  while count < array.length do
+    if array[count] < current_min
+      current_min = array[count]
     end
-    min << value
     count += 1
   end
-  min
+  current_min
 end
+
+def find_min_in_nested_arrays(src)
+  mins = []
+  row_index = 0
+  
+  while row_index < src.length do
+    current_row = src[row_index]
+    current_row_min = min_from_array(current_row)
+    mins << current_row_min
+    row_index += 1
+  end
+  mins
+end
+
+
+
+# def find_min_in_nested_arrays(src)
+#   count = 0
+#   min = []
+#   while count < src.length do
+#     inner_count = 0
+#     value = 1000
+#       if src[count][inner_count] < value
+#       value = src[count][inner_count]
+#       end
+#       inner_count += 1
+#     end
+#     min << value
+#     count += 1
+#   end
+# #  min
+# #end
